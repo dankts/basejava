886bcf10f0aa0e49a05acbcb778293763d4610bc
Daniil Kattsyn <kattsyn.dd@gmail.com>
2022-10-30 14:14:56 +0300

    hw 1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,48 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        storage[size++] = r;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        for (int i = 0; i < size() - 1; i++) {
+            if (Objects.equals(storage[i].uuid, uuid)) {
+                r = storage[i];
+            }
+        }
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size() - 1; i++) {
+            if (storage[i].uuid == uuid) {
+                storage[i] = null;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumesAvailable = Arrays.copyOf(storage, size());
+        return resumesAvailable;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
